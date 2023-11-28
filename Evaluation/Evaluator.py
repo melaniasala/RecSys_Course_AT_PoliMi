1d70db64679d9940c395fea2479a63503d0dc95c
melaniasala <melania.sala@mail.polimi.it>
2023-11-28 20:05:30 +0100

    Fix typo

--- Evaluation/Evaluator.py
+++ Evaluation/Evaluator.py
@@ -687,12 +687,11 @@ class EvaluatorHoldout_for_LightFM(EvaluatorHoldout):
             user_batch_start = user_batch_end
 
             # Compute predictions for a batch of users using vectorization, much more efficient than computing it one at a time
-            recommended_items_batch_list, scores_batch = self.recommend_with_LightFM(recommender_object,
-                                                                                                     self.URM_train,
-                                                                                                     test_user_batch_array,
-                                                                                                     remove_seen_flag=self.exclude_seen,
-                                                                                                     return_scores = True
-                                                                                                     )
+            recommended_items_batch_list, scores_batch = self.recommend_with_LightFM(recommender_object, 
+                                                                                     self.URM_train, 
+                                                                                     test_user_batch_array, 
+                                                                                     remove_seen_flag= True, return_scores = True
+                                                                                    )
 
             results_dict = self._compute_metrics_on_recommendation_list(test_user_batch_array = test_user_batch_array,
                                                          recommended_items_batch_list = recommended_items_batch_list,
